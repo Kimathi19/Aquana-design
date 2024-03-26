6b4b8b280bed12fc79bba5f72c37ca166b65a583
brian <muriikibrian9@gmail.com>
2024-03-26 16:21:02 +0300

    remaking the web pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        card: '#f0f2f5',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
